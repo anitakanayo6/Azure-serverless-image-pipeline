a2e721e9c3f867c8764ad73e986e5f2e93d12b31
Anita Khanayo <kanayoanita04@gmail.com>
2026-08-05 04:28:59 +0200

    docs : "Define methods for image upload and processing"
    Added method stubs and comments for image processing functions.

---
+++ ImageFunction.java
@@ -0,0 +1,7 @@
+// write the java code that will trigger the azure function when the image is aploaded  to the uploads Blob Storage container. 
+
+// Methods :
+// run() - to be triggered when the image is being uploaded to the storage container
+// createThumbnail() - resize the image to thumbnail 
+// uploadThumbnail() - upoload the thumbnail in the blob storage container
+
